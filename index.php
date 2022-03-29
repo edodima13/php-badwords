40faf333155d4626f9968127b27d2c5531fee42d
edodima13 <edodima13@libero.it>
2022-03-29 16:59:45 +0200

    schema esercizio

---
+++ index.php
@@ -0,0 +1,17 @@
+//creare una variabile con file di testo
+//stampare il testo e la sua lunghezza
+//far generare dall'utente una parola da censurare tramite get
+//sostituire con tre (***) la parola da censurare
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
